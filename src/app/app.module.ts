f45003233a2844a212969214354c968e2411021d
IsmailIbrahim84 <beisso75@hotmail.com>
2021-03-05 15:39:19 +0200

    Adding our First Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import {PostCreateComponent} from './Posts/Post-Create/post-create.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule
