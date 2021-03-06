72c81ef49907441e326dae69b6f9c83a557acfd5
IsmailIbrahim84 <beisso75@hotmail.com>
2021-03-06 10:10:20 +0200

    Adding Input and TextArea Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from './Posts/Post-Create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent
   ],
   imports: [
-    BrowserModule,FormsModule, BrowserAnimationsModule
+    BrowserModule,FormsModule, BrowserAnimationsModule,MatInputModule,MatCardModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
