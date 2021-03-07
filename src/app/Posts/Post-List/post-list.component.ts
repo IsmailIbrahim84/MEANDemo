5fba5742c79e963902c978781720f64c7ffe26e1
IsmailIbrahim84 <beisso75@hotmail.com>
2021-03-07 20:09:59 +0200

    Adding structural directives

--- src/app/Posts/Post-List/post-list.component.ts
+++ src/app/Posts/Post-List/post-list.component.ts
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-  post = [
-    {title:"First Post",content:"This is the fist post."},
-    {title:"Second Post",content:"This is the second post."},
-    {title:"Thrid Post",content:"This is the third post."},
-  ]
+  post=[]
+  // post = [
+  //   {title:"First Post",content:"This is the fist post."},
+  //   {title:"Second Post",content:"This is the second post."},
+  //   {title:"Thrid Post",content:"This is the third post."},
+  // ]
   constructor() { }
 
   ngOnInit(): void {
